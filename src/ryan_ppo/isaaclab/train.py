Many updates. Specifically: Fixed usage of .ini config parsing (some variables were retrieving the incorrect information). Changed observation normalization method to be shared between actor and critic. removed KL divergence based early stopping. Changed advantage normalization to be per mini-batch rather than per-rollout. Moved learning rate scheduling to be for current update, not after the epoch. Lastly, fixed algorithm not distinguishing between truncated and terminated for gae bootstrapping.

src/ryan_ppo/isaaclab/train.py
@@ -59,8 +59,8 @@ def train(args_cli):
     learning_rate = float(env_config['train']['learning_rate'])
     gamma = float(env_config['train']['gamma'])
     num_learning_epochs = int(env_config['train']['num_learning_epochs'])
-    desired_kl = float(env_config['train']['gamma'])
-    clip_epsilon = float(env_config['train']['gamma'])
+    desired_kl = float(env_config['train']['desired_kl'])
+    clip_epsilon = float(env_config['train']['clip_epsilon'])
 
     if args_cli.sweep:
 
@@ -202,6 +202,8 @@ def train(args_cli):
             # take step in environment
             next_state, reward, terminated, truncated, info = env.step(action)
             done = torch.logical_or(terminated, truncated)
+            
+            reward = reward + agent.gamma * value * truncated.float()
 
             # store rollout data in tensors
             states[step] = state_obs
@@ -322,22 +324,6 @@ def train(args_cli):
     if (args_cli.save):
         torch.save(agent.actor.state_dict(), log_path + "actor_final.pth")
         torch.save(agent.critic.state_dict(), log_path + "critic_final.pth")
-
-    # # save training data for plotting
-    # np.savez(
-    #     log_path + "training_data.npz",
-    #     timesteps=np.array([x[0] for x in plot_data]),
-    #     avg_rewards=np.array([x[1] for x in plot_data]),
-    #     episode_rewards=np.array(episode_rewards),
-    #     episode_lengths=np.array(episode_lengths),
-    # )
-    # print(f"\n✓ Training data saved to {log_path}training_data.npz")
-
-    # print(f"\nTraining complete! Final model saved.")
-    # print(f"Total episodes: {len(episode_rewards)}")
-    # if episode_rewards:
-    #     print(
-    #         f"Final average reward (last 100 episodes): {np.mean(episode_rewards[-100:]):.2f}")
 
 def get_cfg_path(task):
     from pathlib import Path

src/ryan_ppo/network.py
@@ -15,13 +15,14 @@ class Actor(nn.Module):
                  state_dim: int,
                  action_dim: int,
                  hidden_dims: list[int] = [64, 64],
-                 use_normalization: bool = True,
-                 std: float = 0.3) -> None:
+                 normalization_object: EmpiricalNormalization = None,
+                 std: float = 1.0) -> None:
         super(Actor, self).__init__()
 
-        self.use_normalization = use_normalization
-        if use_normalization:
-            self.obs_normalizer = EmpiricalNormalization(state_dim)
+        if normalization_object:
+            self.obs_normalizer = normalization_object
+        else:
+            self.obs_normalizer = None
 
         layers = []
         prev_dim = state_dim
@@ -47,7 +48,7 @@ class Actor(nn.Module):
 
     def forward(self, x: torch.tensor) -> tuple[torch.tensor, torch.tensor]:
         # normalize observations, then forward pass
-        if self.use_normalization:
+        if self.obs_normalizer:
             x = self.obs_normalizer(x)
 
         for layer in self.hidden_layers:
@@ -58,7 +59,7 @@ class Actor(nn.Module):
 
     def update_normalization(self, obs: torch.tensor) -> None:
         # update observation normalization statistics
-        if self.use_normalization:
+        if self.obs_normalizer:
             self.obs_normalizer.update(obs)
 
 
@@ -67,12 +68,13 @@ class Critic(nn.Module):
     def __init__(self,
                  state_dim: int,
                  hidden_dims: list[int] = [64, 64],
-                 use_normalization: bool = True) -> None:
+                 normalization_object: EmpiricalNormalization = None) -> None:
         super(Critic, self).__init__()
 
-        self.use_normalization = use_normalization
-        if use_normalization:
-            self.obs_normalizer = EmpiricalNormalization(state_dim)
+        if normalization_object:
+            self.obs_normalizer = normalization_object
+        else:
+            self.obs_normalizer = None
 
         layers = []
         prev_dim = state_dim
@@ -96,7 +98,7 @@ class Critic(nn.Module):
                 nn.init.constant_(module.bias, 0.0)
 
     def forward(self, x: torch.tensor) -> torch.tensor:
-        if self.use_normalization:
+        if self.obs_normalizer:
             x = self.obs_normalizer(x)
 
         for layer in self.hidden_layers:
@@ -106,5 +108,5 @@ class Critic(nn.Module):
 
     def update_normalization(self, obs: torch.tensor) -> None:
         # update observation normalization statistics
-        if self.use_normalization:
+        if self.obs_normalizer:
             self.obs_normalizer.update(obs)

src/ryan_ppo/ppo.py
@@ -7,6 +7,8 @@ import torch.optim as optim
 
 import numpy as np
 
+from rsl_rl.modules import EmpiricalNormalization
+
 from ryan_ppo.network import Actor, Critic
 
 
@@ -28,10 +30,13 @@ class PPOAgent:
                  entropy_coef: float = 0.001,
                  use_normalization: bool = True) -> None:
 
+        if use_normalization:
+            self.obs_normalizer = EmpiricalNormalization(state_dim)
+
         # initialization of networks and optimizer
         self.device = device
-        self.actor = Actor(state_dim, action_dim, hidden_dims).to(device)
-        self.critic = Critic(state_dim, hidden_dims).to(device)
+        self.actor = Actor(state_dim, action_dim, hidden_dims, self.obs_normalizer).to(device)
+        self.critic = Critic(state_dim, hidden_dims, self.obs_normalizer).to(device)
         self.optimizer = optim.Adam(
             list(self.actor.parameters()) + list(self.critic.parameters()), lr=lr)
 
@@ -93,8 +98,8 @@ class PPOAgent:
 
         returns = advantages + values
 
-        advantages = (advantages - advantages.mean()) / \
-            (advantages.std() + 1e-8)
+        # advantages = (advantages - advantages.mean()) / \
+        #     (advantages.std() + 1e-8)
 
         return advantages, returns
 
@@ -132,8 +137,8 @@ class PPOAgent:
         for epoch in range(epochs):
 
             # early stopping for KL divergence
-            if stop_early:
-                break
+            # if stop_early:
+            #     break
 
             # randomizes batch data
             indices = np.random.permutation(dataset_size)
@@ -151,6 +156,9 @@ class PPOAgent:
                 batch_values_old = b_values_old[batch_indices]
                 batch_mus_old = b_mus_old[batch_indices]
                 batch_stds_old = b_stds_old[batch_indices]
+
+                batch_advantages = (batch_advantages - batch_advantages.mean()) / \
+                (batch_advantages.std() + 1e-8)
 
                 # calculate log_probs for current policy
                 mu, std = self.actor(batch_states)
@@ -173,11 +181,19 @@ class PPOAgent:
                     mean_kl += batch_kl
                     num_updates += 1
 
+                    if self.schedule_type == "adaptive":
+                        if batch_kl > self.desired_kl * 2.0:
+                            self.current_lr = max(1e-5, self.current_lr / 1.5)
+                        elif batch_kl < self.desired_kl / 2.0 and batch_kl > 0.0:
+                            self.current_lr = min(1e-2, self.current_lr * 1.5)
+                        for param_group in self.optimizer.param_groups:
+                            param_group['lr'] = self.current_lr
+
                     # early stopping: if KL has already exceeded the threshold, skip
                     # remaining mini-batches and epochs to avoid over-updating the policy
-                    if batch_kl > self.desired_kl * 2.0:
-                        stop_early = True
-                        break
+                    # if batch_kl > self.desired_kl * 2.0:
+                    #     # stop_early = True
+                    #     continue
 
                 # compute surrogate loss
                 ratios = torch.exp(log_probs - batch_log_probs_old)
@@ -216,12 +232,5 @@ class PPOAgent:
         # average KL divergence over all updates, adjust learning rate if using adaptive schedule
         mean_kl = mean_kl / num_updates if num_updates > 0 else 0
         self.update_count += 1
-        if self.schedule_type == "adaptive":
-            if mean_kl > self.desired_kl * 2.0:
-                self.current_lr = max(1e-5, self.current_lr / 1.5)
-            elif mean_kl < self.desired_kl / 2.0 and mean_kl > 0.0:
-                self.current_lr = min(1e-2, self.current_lr * 1.5)
-            for param_group in self.optimizer.param_groups:
-                param_group['lr'] = self.current_lr
 
         return mean_kl