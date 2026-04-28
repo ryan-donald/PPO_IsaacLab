1d1c3052ab6f707c4e5baab76d3c2f1e92958e14
ryan <ryanpdonald@gmail.com>
2026-04-28 17:16:26 -0400

    Edited project data that was conflicting between the setup.py and pyproject.toml files with github actions for automatic testing.

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="ryan-ppo",
-    version="0.1.0",
-    author="Ryan Donald",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    python_requires=">=3.10",
 )
