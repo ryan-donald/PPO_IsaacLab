Further optimized the training script, can now run over 600k sps with my so101 reach task on 3070. Re-added KL based early stopping for updates. Removed obsolete env_cfgs.py file, various other fixes.

src/ryan_ppo/isaaclab/train.py
@@ -130,7 +130,6 @@ def train(args_cli):
     # logging and checkpointing
     log_path = f"ppo_logs/{args_cli.task}/{run_id}/"
     os.makedirs(log_path, exist_ok=True)
-    log_path + "actor_final.pth"
 
     # if os.path.exists(checkpoint_path):
     #     print(f"\nFound existing checkpoint: {checkpoint_path}")
@@ -159,6 +158,7 @@ def train(args_cli):
     last_min_reward = 0.0
     last_max_reward = 0.0
     last_std_reward = 0.0
+    last_avg_entropy = 0.0
     last_term_rewards = {name: 0.0 for name in term_names}
 
     stats = {
@@ -217,18 +217,14 @@ def train(args_cli):
             # update normalization statistics
             if env_config["train"]["use_normalization"] == "True":
                 agent.actor.update_normalization(state_obs)
-                agent.critic.update_normalization(state_obs)
 
             if args_cli.profile:
                 carb.profiler.begin(2, "select_action")
 
             # select action from policy
             with torch.no_grad():
-                action, log_prob, entropy = agent.select_action(state_obs)
+                action, log_prob, entropy, mu, std = agent.select_action(state_obs)
                 value = agent.critic(state_obs).squeeze()
-
-                # get mu and std for KL divergence computation
-                mu, std = agent.actor(state_obs)
 
             if args_cli.profile:
                 carb.profiler.end(2)
@@ -312,6 +308,7 @@ def train(args_cli):
             stds,
             epochs=num_learning_epochs,
             batch_size=batch_size,
+            num_mini_batches=num_mini_batches,
         )
 
         if args_cli.profile:
@@ -335,11 +332,15 @@ def train(args_cli):
             max_reward = completed_rewards.max().item()
             std_reward = completed_rewards.std().item() if num_completed > 1 else 0.0
 
+            avg_entropy = entropies.mean().item()
+
             # update trackers
             last_avg_reward = avg_reward
             last_min_reward = min_reward
             last_max_reward = max_reward
             last_std_reward = std_reward
+
+            last_avg_entropy = avg_entropy
 
             for t_idx, t_name in enumerate(term_names):
                 completed_terms = historic_term_rewards[:, :, t_idx][done_mask]
@@ -353,6 +354,8 @@ def train(args_cli):
             max_reward = last_max_reward
             std_reward = last_std_reward
 
+            avg_entropy = last_avg_entropy
+
         logging_dict = {
             "train/avg_reward": avg_reward,
             "train/min_reward": min_reward,
@@ -361,6 +364,7 @@ def train(args_cli):
             "train/kl": mean_kl,
             "train/lr": agent.current_lr,
             "train/episodes": num_episodes_completed,
+            "train/avg_entropy": avg_entropy,
         }
 
         for t_name in term_names:
@@ -426,7 +430,7 @@ def get_cfg_path(task):
 
     current_file_path = Path(__file__).resolve()
     project_root = current_file_path.parents[3]
-    ini_file_path = project_root / "cfg" / f"{args_cli.task}.ini"
+    ini_file_path = project_root / "cfg" / f"{task}.ini"
     if not ini_file_path.exists():
         raise FileNotFoundError(f"Configuration file not found at: {ini_file_path}")
 

src/ryan_ppo/ppo.py
@@ -29,6 +29,8 @@ class PPOAgent:
 
         if use_normalization:
             self.obs_normalizer = ObsNormalization(state_dim)
+        else:
+            self.obs_normalizer = None
 
         # initialization of networks and optimizer
         self.device = device
@@ -36,9 +38,11 @@ class PPOAgent:
             device
         )
         self.critic = Critic(state_dim, hidden_dims, self.obs_normalizer).to(device)
-        self.optimizer = optim.Adam(
-            list(self.actor.parameters()) + list(self.critic.parameters()), lr=lr
-        )
+
+        self.actor_params = list(self.actor.parameters())
+        self.critic_params = list(self.critic.parameters())
+
+        self.optimizer = optim.Adam(self.actor_params + self.critic_params, lr=lr)
 
         self.actor = torch.compile(self.actor)
         self.critic = torch.compile(self.critic)
@@ -73,7 +77,7 @@ class PPOAgent:
         action = dist.sample()
         entropy = dist.entropy().sum(dim=-1)
         log_prob = dist.log_prob(action).sum(dim=-1)
-        return action, log_prob, entropy
+        return action, log_prob, entropy, mu, std
 
     @torch.compile
     def compute_gae(
@@ -103,9 +107,6 @@ class PPOAgent:
 
         returns = advantages + values
 
-        # advantages = (advantages - advantages.mean()) / \
-        #     (advantages.std() + 1e-8)
-
         return advantages, returns
 
     # @torch.compile
@@ -121,6 +122,7 @@ class PPOAgent:
         stds_old: torch.Tensor,
         epochs: int = 4,
         batch_size: int = 64,
+        num_mini_batches: int = 4,
     ) -> float:
         # updates Actor and Critic networks using the PPO algorithm
 
@@ -136,17 +138,15 @@ class PPOAgent:
 
         dataset_size = b_states.shape[0]
 
-        mean_kl = 0.0
+        mean_kl = 0
         num_updates = 0
 
         # training loop
         for epoch in range(epochs):
-            # early stopping for KL divergence
-            # if stop_early:
-            #     break
-
             # randomizes batch data
             indices = torch.randperm(dataset_size, device=self.device)
+
+            epoch_kl = 0
 
             # mini-batch updates
             for start in range(0, dataset_size, batch_size):
@@ -176,7 +176,7 @@ class PPOAgent:
                 # full KL divergence
                 with torch.no_grad():
                     kl = torch.sum(
-                        torch.log(std / (batch_stds_old + 1e-8) + 1e-8)
+                        torch.log(std / (batch_stds_old + 1e-8))
                         + (
                             torch.square(batch_stds_old)
                             + torch.square(batch_mus_old - mu)
@@ -186,7 +186,7 @@ class PPOAgent:
                         dim=-1,
                     )
                     batch_kl = kl.mean().item()
-                    mean_kl += batch_kl
+                    epoch_kl += batch_kl
                     num_updates += 1
 
                     if self.schedule_type == "adaptive":
@@ -196,13 +196,6 @@ class PPOAgent:
                             self.current_lr = min(1e-2, self.current_lr * 1.5)
                         for param_group in self.optimizer.param_groups:
                             param_group["lr"] = self.current_lr
-
-                    # early stopping: if KL has already exceeded the threshold, skip
-                    # remaining mini-batches and epochs to avoid
-                    # over-updating the policy
-                    # if batch_kl > self.desired_kl * 2.0:
-                    #     # stop_early = True
-                    #     continue
 
                 # compute surrogate loss
                 ratios = torch.exp(log_probs - batch_log_probs_old)
@@ -236,10 +229,15 @@ class PPOAgent:
                 loss.backward()
 
                 torch.nn.utils.clip_grad_norm_(
-                    list(self.actor.parameters()) + list(self.critic.parameters()),
+                    self.actor_params + self.critic_params,
                     self.max_grad_norm,
                 )
                 self.optimizer.step()
+            mean_kl += epoch_kl
+
+            mean_epoch_kl = epoch_kl / num_mini_batches
+            if mean_epoch_kl > self.desired_kl * 1.5:
+                break
 
         # average KL divergence over all updates,
         # adjust learning rate if using adaptive schedule